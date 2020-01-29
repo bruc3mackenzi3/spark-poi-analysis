86f0875aae27e86e016205b49618f69e2236a3bd
Bruce MacKenzie <mackenzbb@gmail.com>
2020-01-28 20:28:38 -0500

    Variable rename, commenting

--- data/aggregate_poi.py
+++ data/aggregate_poi.py
@@ -10,8 +10,11 @@ APP_NAME = 'POI_AGGREGATION'
 
 class DataSource:
     def __init__(self):
+        ''' Constructor initializing Spark environment
+        '''
+
         self.poi_list = None
-        self.request_df = None
+        self.req_df = None
 
         conf = SparkConf().setAppName(APP_NAME)
         conf = conf.setMaster('local[*]')
@@ -19,6 +22,11 @@ class DataSource:
         self.spark = SparkSession.builder.config(conf=conf).getOrCreate()
 
     def get_data(self):
+        ''' Load request and POI data from CSVs.
+            self.req_df stores request data as a Spark DataFrame
+            self.poi_list contains the POI data as a list
+        '''
+
         # Load POI List
         self.poi_list = []
         with open('/tmp/data/POIList.csv') as f:
@@ -29,29 +37,41 @@ class DataSource:
         print('POI List:', self.poi_list)
 
         # Load request data and filter bad requests
-        self.request_df = self.spark.read.csv('/tmp/data/DataSample.csv', inferSchema='true', header='true')
-            # count 22025
-        print('Request data loaded.  Number of records:', self.request_df.count())
-        print(self.request_df.dtypes)
+        self.req_df = self.spark.read.csv('/tmp/data/DataSample.csv',
+                                          inferSchema='true', header='true')
+        print('Request data loaded.  Number of records:', self.req_df.count())
+        print(self.req_df.dtypes)
 
     def cleanup_data(self):
-        groupedCoordDf = self.request_df.select('*') \
-                .groupBy(self.request_df.Latitude,self.request_df.Longitude,self.request_df.TimeSt) \
+        ''' Clean data by removing suspected malicious requests.  Such requests
+            contain identical Latitude, Longitude and TimeSt values.
+            Precondition: Data has been loaded with self.get_data
+        '''
+
+        raw_count = self.req_df.count()
+        # Create new DataFrame only containing filtered requests
+        groupedCoordDf = self.req_df.select('*') \
+                .groupBy(
+                    self.req_df.Latitude,
+                    self.req_df.Longitude,
+                    self.req_df.TimeSt) \
                 .count() \
                 .filter('count == 1') \
                 .drop('count')
-            # count 17973
 
-        self.request_df = self.request_df.join(groupedCoordDf, ['Latitude','Longitude', 'TimeSt'])
-        print('Size of joined data:', self.request_df.count())
+        # Perform an inner join so flagged records are omitted
+        self.req_df = self.req_df.join(groupedCoordDf, ['Latitude','Longitude', 'TimeSt'])
+        filtered_count = self.req_df.count()
+        print('Size of joined data: {}, number of records removed: {}'.format(
+                filtered_count, raw_count-filtered_count))
 
     def label_data(self):
-        ''' Label request data with the ID of the nearest POI
+        ''' Label each record in request data with the nearest POI
         '''
 
-        # Given the request's and each POI's coordinates calculate each distance
-        # and returns the ID of the nearest POI
-        # Note: Uses Vincenty's formula from geopy library
+        # Given a request and all POI coordinates calculate each distance
+        # and returns the nearest POI's ID
+        # Note: Uses Vincenty's formula from geopy library to calculate distance
         def calc_nearest_poi(req_coord, poi_list):
             min_distance = []
             for poi in poi_list:
@@ -66,10 +86,15 @@ class DataSource:
             return functions.udf(lambda vals: calc_nearest_poi(vals, poi_list))
 
         # Add new column to request data with the name of the nearest POI
-        self.request_df = self.request_df.withColumn('NearestPOI', udf_label(self.poi_list)(functions.array('Latitude','Longitude')))
-        self.request_df.groupBy('NearestPOI').count().show()
+        self.req_df = self.req_df.withColumn('NearestPOI', udf_label(self.poi_list)(functions.array('Latitude','Longitude')))
+        self.req_df.groupBy('NearestPOI').count().show()
 
     def calc_poi_distance(self):
+        ''' Given a request and the nearest POI calculate and return the
+            distance
+        '''
+
+        # Note: Follows the same pattern of wrapping in a UDF as above
         def calc_distance(req_vals, poi_list):
             nearest_poi = req_vals[0]
             req_coord = req_vals[1:]
@@ -83,10 +108,14 @@ class DataSource:
             return functions.udf(lambda vals: calc_distance(vals, poi_list), types.DoubleType())
 
         # Add new column to request data with the calculated nearest POI
-        self.request_df = self.request_df.withColumn('POIDistance', udf_label(self.poi_list)(functions.array('NearestPOI','Latitude','Longitude')))
+        self.req_df = self.req_df.withColumn('POIDistance', udf_label(self.poi_list)(functions.array('NearestPOI','Latitude','Longitude')))
 
     def analyze_data(self):
-        gdf = self.request_df.groupBy(self.request_df.NearestPOI)
+        ''' Aggregate request data by nearest POI and calculate mean and
+            standard deviation
+        '''
+
+        gdf = self.req_df.groupBy(self.req_df.NearestPOI)
         gdf.agg(functions.mean('POIDistance').alias('AvgDistance'), functions.stddev('POIDistance').alias('StddevDistance')).show()
 
 
@@ -98,7 +127,8 @@ def main():
     data_source.calc_poi_distance()
     data_source.analyze_data()
 
-    data_source.request_df.show(3)
+    data_source.req_df.show(3)
+
 
 if __name__ == '__main__':
     main()
